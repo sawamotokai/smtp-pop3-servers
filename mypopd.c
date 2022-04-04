829c0c790ee8653298de73fdab00dad0be55bac4
Kai Sawamoto <kaisawamoto@gmail.com>
2022-04-04 15:42:30 -0700

    made state

--- mypopd.c
+++ mypopd.c
@@ -11,50 +11,75 @@
 
 static void handle_client(int fd);
 
-int main(int argc, char *argv[]) {
+struct serverState
+{
+    int authenticated;
+    int locked;
+    mail_list_t emails;
+} state = {0, 0, NULL};
 
-    if (argc != 2) {
-	    fprintf(stderr, "Invalid arguments. Expected: %s <port>\n", argv[0]);
-	    return 1;
+int main(int argc, char *argv[])
+{
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "Invalid arguments. Expected: %s <port>\n", argv[0]);
+        return 1;
     }
     run_server(argv[1], handle_client);
     return 0;
 }
 
-void user(int fd, char *parts[], int argCount) {
+void user(int fd, char *parts[], int argCount)
+{
     printf("USER\n");
-    if (argCount != 2) {
+    if (argCount != 2)
+    {
         send_formatted(fd, "-ERR Syntax error in parameters or arguments\r\n");
         return;
     }
-    if(is_valid_user(parts[1], NULL))
+    if (state.locked)
+    {
+        send_formatted(fd, "-ERR maildrop is locked\r\n");
+        return;
+    }
+    if (is_valid_user(parts[1], NULL))
+    {
+        state.authenticated = 1;
+        state.locked = 1;
+        state.emails = load_user_mail(parts[1]);
         send_formatted(fd, "+OK User accepted\r\n");
-    else    
+    }
+    else
         send_formatted(fd, "-ERR no mailbox for %s here\r\n", parts[1]);
 }
 
-void pass(int fd, char *password) {
+void pass(int fd, char *password)
+{
     printf("PASS\n");
     send_formatted(fd, "+OK maildrop has x messages\r\n");
 }
 
-void stat(int fd, char *args[]) {
+void stat(int fd, char *args[])
+{
     printf("STAT\n");
     if (args[1] != NULL)
     {
         send_formatted(fd, "-ERR invalid arguments\r\n");
         return;
     }
-    
+
     send_formatted(fd, "+OK 2 320");
 }
 
-void list(int fd) {
+void list(int fd)
+{
     printf("LIST\n");
     send_formatted(fd, "+OK maildrop has x messages\r\n");
 }
 
-void retr(int fd, char *args[]) {
+void retr(int fd, char *args[])
+{
     printf("RETR\n");
     if (args[1] == NULL)
     {
@@ -64,7 +89,8 @@ void retr(int fd, char *args[]) {
     send_formatted(fd, "+OK maildrop has x messages\r\n");
 }
 
-void dele(int fd, char *args[]) {
+void dele(int fd, char *args[])
+{
     printf("DELE\n");
     if (args[1] == NULL)
     {
@@ -74,32 +100,38 @@ void dele(int fd, char *args[]) {
     send_formatted(fd, "+OK maildrop has x messages\r\n");
 }
 
-void noop(int fd) {
+void noop(int fd)
+{
     printf("NOOP\n");
     send_formatted(fd, "+OK maildrop has x messages\r\n");
 }
 
-void rset(int fd) {
+void rset(int fd)
+{
     printf("RSET\n");
     send_formatted(fd, "+OK maildrop has x messages\r\n");
 }
 
-void quit(int fd) {
+void quit(int fd)
+{
     printf("QUIT\n");
     send_formatted(fd, "+OK dewey POP3 server signing off\r\n");
-} 
+}
 
-void initRes(int fd) {
+void initRes(int fd)
+{
     printf("INIT RES\n");
     send_formatted(fd, "+OK dewey POP3 server signing in\r\n");
 }
 
-void errRes(int fd) {
+void errRes(int fd)
+{
     printf("ERR RES\n");
     send_formatted(fd, "-ERR dewey POP3 server signing in\r\n");
 }
 
-void handle_client(int fd) {
+void handle_client(int fd)
+{
     char recvbuf[MAX_LINE_LENGTH + 1];
     net_buffer_t nb = nb_create(fd, MAX_LINE_LENGTH);
     /* TO BE COMPLETED BY THE STUDENT */
@@ -115,7 +147,6 @@ void handle_client(int fd) {
         int argCount = split(recvbuf, parts);
         char *command = parts[0];
 
-        
         if (strcasecmp(command, "USER") == 0)
             user(fd, parts, argCount);
         else if (strcasecmp(command, "PASS") == 0)
